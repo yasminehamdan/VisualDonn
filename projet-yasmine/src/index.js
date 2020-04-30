94f8d8ca7f3f25c12da1eda7d37ee5a9233f571d
JeanHerbaut <jean.herbaut@heig-vd.ch>
2020-04-30 23:40:41 +0200

    arborescence maj

---
+++ projet-yasmine/src/index.js
@@ -0,0 +1,10 @@
+//js
+import grapheNationalities from './js/grapheNationalities/index'
+import graphContinent from './js/graphContinent'
+import graphe20172019 from './js/graphe2017-2019'
+console.log("test")
+
+// utiliser les fonctions importées
+grapheNationalities()
+graphContinent()
+graphe20172019()
