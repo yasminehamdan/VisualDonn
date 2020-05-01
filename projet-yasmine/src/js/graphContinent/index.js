e3f382fe787086bb17ff37d459b426a2446ee43c
Yasmine Hamdan <yasmine.hamdan@heig-vd.ch>
2020-05-01 09:35:48 +0200

    màj

--- projet-yasmine/src/js/graphContinent/index.js
+++ projet-yasmine/src/js/graphContinent/index.js
@@ -1,26 +1,34 @@
 import bb from 'billboard.js'
 import data from '../../../data/continents.json'
 
+let suisses = data.map(d => d.suisses)
+let continent= data.map(d => d.continent)
+
 export default function () {
 
-  // let chart = bb.generate({
-  //     data: {
-  //       json: {
-  //         population: resultat.map(({ suisses }) => suisses),
-  //       },
-  //       type: 'pie',
-  //       onover: function(d, i) {
-  //         console.log("onover", d, i);
-  //          },
-  //       onout: function(d, i) {
-  //         console.log("onout", d, i);
-  //       },
-  //     axis: {
-  //       x: {
-  //         type: 'category',
-  //         categories: resultat.map(({ continent }) => continent),
-  //       }
-  //     },
-  //     bindto: document.getElementById('graphique')
-  //   })
-  }
+  let tab = []
+
+  continent.forEach((continent, key) => {
+     tab.push([continent, suisses[key]])
+  });
+  console.log(tab)
+  let chart = bb.generate({
+    data: {
+      columns: tab,
+      type: 'pie',
+      onover: function(d, i) {
+        console.log("onover", d, i);
+         },
+      onout: function(d, i) {
+        console.log("onout", d, i);
+      },
+    axis: {
+        x: {
+          type: 'category',
+          categories: tab.map(({ continent }) => continent),
+        },
+    bindto: "#continent-graphique"
+    }}});
+
+}
+  
--- projet-yasmine/src/js/grapheNationalities/index.js
+++ projet-yasmine/src/js/grapheNationalities/index.js
@@ -1,24 +1,28 @@
 import bb from 'billboard.js'
 import data from '../../../data/25pays.json'
- 
-let colones = data.map(d => d.suisses)
-let pays = data.map(d => d.pays)
-console.log(colones)
+
+let colonnes = data.map(d => d.suisses)
+let pays= data.map(d => d.pays)
+
+
 export default function() {
- 
+  let tab = []
+
+  pays.forEach((pays, key) => {
+     tab.push([pays, colonnes[key]])
+  });
+  console.log(tab)
   let chart = bb.generate({
     data: {
-      columns: [
-    colones
-      ],
+      columns: tab,
       type: "bar"
     },
     bar: {
       width: {
         ratio: 0.5
-      },
+      }
     },
     bindto: "#nationalities-graphique"
   });
- 
+
 }
